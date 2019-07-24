4361d05142a19509ca858e4eff6039dee03a0496
it137678620 <53246987+it137678620@users.noreply.github.com>
2019-07-24 17:07:37 +0800

    Update demo01.java
    222

--- demo01/src/main/java/com/czxy/demo01.java
+++ demo01/src/main/java/com/czxy/demo01.java
@@ -9,7 +9,7 @@ public class demo01 {
     public static void main(String[] args) {
         System.out.println(111);
         System.out.println(333);
-        System.out.println(444);
+ 
         System.out.println(555);
     }
 }
