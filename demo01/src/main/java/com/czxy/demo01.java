eb931d059604d96c64b360016c50142326ea27d1
it137678620 <53246987+it137678620@users.noreply.github.com>
2019-07-24 17:16:55 +0800

    Update demo01.java
    3

--- demo01/src/main/java/com/czxy/demo01.java
+++ demo01/src/main/java/com/czxy/demo01.java
@@ -7,7 +7,7 @@ package com.czxy;
  */
 public class demo01 {
     public static void main(String[] args) {
-        System.out.println(222);
+        System.out.println(222111);
   
         System.out.println(777);
     }
