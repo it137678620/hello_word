56b1400271fc78af9dee75eed545720056e14b15
it137678620 <137678620@qq.com>
2019-07-24 17:15:09 +0800

    demo01方法修改222

--- demo01/src/main/java/com/czxy/demo01.java
+++ demo01/src/main/java/com/czxy/demo01.java
@@ -13,6 +13,12 @@ public class demo01 {
         System.out.println(444);
         System.out.println(555);
         System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
         System.out.println(777);
     }
 }
